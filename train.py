565706fdaaef8d08b35ba4c10a77ac5a8d47a6ea
Jonathan Coney <joncon@noc.ac.uk>
2025-07-29 11:47:46 +0100

    tweaked training script for 4d data

--- train.py
+++ train.py
@@ -1,118 +1,16 @@
-import glob
-import os
 import time
-from math import prod
-from random import randint
 
-import hydra
-import kornia.filters as kfilts
 import numpy as np
 import pytorch_lightning as L
 import torch
 import torch.nn.functional as F
 import xarray as xr
-
-# from ocean4dvarnet.data import XrConcatDataset
-# from ocean4dvarnet.models import ConvLstmGradModel
-from ocean4dvarnet.models import (
-    BaseObsCost,
-    BilinAEPriorCost,
-    ConvLstmGradModel,
-    GradSolver,
-    Lit4dVarNet,
-)
-from ocean4dvarnet.utils import cosanneal_lr_adam, get_triang_time_wei
 from pytorch_lightning import (
-    Callback,
     LightningDataModule,
     Trainer,
 )
-from pytorch_lightning.callbacks import ModelCheckpoint
 from torch import nn
-from torch.utils.data import DataLoader, Dataset
-
-
-class Lit4dVarNetIgnoreNaN(Lit4dVarNet):
-    def __init__(self, *args, **kwargs):
-        _val_rec_weight = kwargs.pop(
-            "val_rec_weight",
-            kwargs["rec_weight"],
-        )
-        super().__init__(*args, **kwargs)
-
-        self.register_buffer(
-            "val_rec_weight",
-            torch.from_numpy(_val_rec_weight),
-            persistent=False,
-        )
-
-        self._n_rejected_batches = 0
-
-    def get_rec_weight(self, phase):
-        rec_weight = self.rec_weight
-        if phase == "val":
-            rec_weight = self.val_rec_weight
-        return rec_weight
-
-    def training_step(self, batch, batch_idx):
-        loss = super().training_step(batch, batch_idx)
-        # print(loss)
-        if loss is None:
-            self._n_rejected_batches += 1
-        return loss
-
-    def on_train_epoch_end(self):
-        self.log(
-            "n_rejected_batches",
-            self._n_rejected_batches,
-            on_step=False,
-            on_epoch=True,
-        )
-
-    def step(self, batch, phase):
-        if self.training and batch.tgt.isfinite().float().mean() < 0.5:
-            print("bother")
-            return None, None
-
-        loss, out = self.base_step(batch, phase)
-        grad_loss = self.weighted_mse(
-            kfilts.sobel(out) - kfilts.sobel(batch.tgt),
-            self.get_rec_weight(phase),
-        )
-
-        prior_cost = self.solver.prior_cost(self.solver.init_state(batch, out))
-        self.log(
-            f"{phase}_gloss",
-            grad_loss,
-            prog_bar=False,
-            on_step=False,
-            on_epoch=True,  # sync_dist=True,
-        )
-
-        training_loss = 50 * loss + 1000 * grad_loss + 1.0 * prior_cost
-        return training_loss, out
-
-    def base_step(self, batch, phase):
-        out = self(batch=batch)
-        loss = self.weighted_mse(out - batch.tgt, self.get_rec_weight(phase))
-
-        with torch.no_grad():
-            self.log(
-                f"{phase}_mse",
-                10000 * loss * self.norm_stats[phase][1] ** 2,
-                prog_bar=True,
-                on_step=False,
-                on_epoch=True,  # sync_dist=True,
-            )
-            self.log(
-                f"{phase}_loss",
-                loss,
-                prog_bar=False,
-                on_step=False,
-                on_epoch=True,  # sync_dist=True,
-            )
-
-        return loss, out
+from torch.utils.data import DataLoader
 
 
 class XrDataset(torch.utils.data.Dataset):
@@ -260,68 +158,14 @@ class XrDataModule(LightningDataModule):
         return dataloader
 
 
-inp = xr.open_dataset("/home/joncon/SORTED/ARGO_gridded_20182024_ro.nc")["TEMPERATURE"]
-tgt = xr.open_dataset("/home/joncon/SORTED/RG_ARGO_NA_20042024_ro.nc")["TEMPERATURE"]
-
-
-model_checkpoint = ModelCheckpoint(
-    monitor="val_loss",
-    dirpath="./",
-    filename="best-unsplit20250725_no-sst",
-)
-
-trainer = Trainer(
-    max_epochs=500,
-    callbacks=[model_checkpoint],
-    log_every_n_steps=12,
-    accelerator="auto",
-    precision=32,
-    strategy="ddp_find_unused_parameters_true",
-    # accelerator="tpu", devices=8
-)
-
-tds = XrDataset(
-    inp_da=inp,
-    tgt_da=tgt,
-    patch_dims={"time": 12, "lat": 14, "lon": 38},
-    slice={
-        "time": np.arange(
-            np.datetime64("2018-01-01"),
-            np.datetime64("2024-01-01"),
-            np.timedelta64(1, "M"),
-            dtype="datetime64[M]",
-        )
-    },
-)
-tdl = DataLoader(
-    dataset=XrDataset(
-        inp_da=inp,
-        tgt_da=tgt,
-        patch_dims={"time": 12, "lat": 14, "lon": 38},
-        slice={
-            "time": np.arange(
-                np.datetime64("2018-01-01"),
-                np.datetime64("2024-01-01"),
-                np.timedelta64(1, "M"),
-                dtype="datetime64[M]",
-            )
-        },
-    )
-)
-
-inp_da = xr.ones_like(inp)
-tgt_da = xr.ones_like(tgt)
-print(tgt.dtype)
-
-# rng = np.random.default_rng()
-# inp_da.values = rng.random(size=inp_da.shape, dtype="float32")
-# tgt_da.values = rng.random(size=tgt_da.shape, dtype="float32")
+inp = xr.open_dataset("/home/joncon/SORTED/ARGO_gridded_20182024_3D.nc")["TEMPERATURE"]
+tgt = xr.open_dataset("/home/joncon/SORTED/RG_ARGO_NA_20042024_3D.nc")["TEMPERATURE"]
 
 
 datamodule = XrDataModule(
-    inp_da=inp_da,
-    tgt_da=tgt_da,
-    tpatch_dims={"time": 12, "lat": 14, "lon": 38},
+    inp_da=inp,
+    tgt_da=tgt,
+    tpatch_dims={"time": 12, "pres": 14, "lat": 14, "lon": 38},
     tslice={
         "time": np.arange(
             np.datetime64("2018-01-01"),
@@ -349,42 +193,42 @@ class Encode(nn.Module):
     def __init__(self):
         super().__init__()
         self.Prior_Cost = nn.Sequential(
-            nn.Conv2d(12, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.Conv3d(12, 256, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1)),
             nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.Conv3d(256, 256, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1)),
             nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.Conv3d(256, 256, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1)),
             nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.Conv3d(256, 256, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1)),
             nn.ReLU(),
-            nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.Conv3d(256, 512, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1)),
             nn.ReLU(),
-            nn.Conv2d(512, 12, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.AvgPool2d(kernel_size=2, stride=2, padding=0),
-            nn.UpsamplingBilinear2d(scale_factor=2),
+            nn.Conv3d(512, 12, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1)),
+            nn.AvgPool3d(kernel_size=2, stride=2, padding=0),
+            nn.Upsample(scale_factor=2, mode="trilinear"),
         )
 
     def forward(self, x):
         return self.Prior_Cost(x)
 
 
-class ConvLSTMGradModel(nn.Module):
+class ConvGradModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.ConvLSTM = nn.Sequential(
-            nn.Conv2d(12, 268, kernel_size=(1, 1)),
+        self.ConvNet = nn.Sequential(
+            nn.Conv3d(12, 268, kernel_size=(1, 1, 1)),
             nn.ReLU(),
-            nn.Conv2d(268, 1024, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.Conv3d(268, 1024, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1)),
             nn.ReLU(),
-            nn.Conv2d(1024, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            nn.Conv2d(256, 12, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.Conv3d(1024, 256, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1)),
+            nn.Conv3d(256, 12, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1)),
             nn.ReLU(),
             nn.Dropout(p=0.1, inplace=False),
             nn.Identity(),
         )
 
     def forward(self, x):
-        return self.ConvLSTM(x)
+        return self.ConvNet(x)
 
 
 class FourDVarNet(L.LightningModule):
@@ -428,18 +272,19 @@ class FourDVarNet(L.LightningModule):
         return optimizer
 
 
-model = FourDVarNet(Encode(), ConvLSTMGradModel())
+model = FourDVarNet(Encode(), ConvGradModel())
 
 
 es = L.callbacks.early_stopping.EarlyStopping(
     monitor="val_loss", mode="min", patience=3
 )
 
-trainer = L.Trainer(
+trainer = Trainer(
     default_root_dir="~/SORTED/4dvarnet/ocean4dvarnet/logging/"
     + time.strftime("%Y%m%d_%H%M%S", time.gmtime())
     + "/",
     callbacks=[es],
     max_epochs=100,
+    log_every_n_steps=6,
 )
 trainer.fit(model=model, datamodule=datamodule)
